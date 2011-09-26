04141249f30a393d449cc593805cf8a89499f982
illiatdesdindes <illiatdesdindes@gmail.com>
2011-09-26 16:25:16 +0200

    install active admin and resource evenement

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Phandicap::Application.routes.draw do
   
   
 
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   get "pages/notadmin", :as => :notadmin
 
   match "cartes" => "cartes#index", :as => :cartes
